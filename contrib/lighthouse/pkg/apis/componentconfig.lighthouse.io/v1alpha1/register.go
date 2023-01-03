8606743646e528c97017458aaef7d9a0655653cf
Puxuan <leslie.qq.lol@gmail.com>
2023-01-03 11:58:38 +0100

    adding missing dependency

--- contrib/lighthouse/pkg/apis/componentconfig.lighthouse.io/v1alpha1/register.go
+++ contrib/lighthouse/pkg/apis/componentconfig.lighthouse.io/v1alpha1/register.go
@@ -16,6 +16,7 @@
 package v1alpha1
 
 import (
+	"github.com/Interstellarss/lighthouse/pkg/apis/componentconfig.lighthouse.io"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
